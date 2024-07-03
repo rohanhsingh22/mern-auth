ec6467a949808b892189d725d182a48e121025ea
Rohan Singh <riskysingh897@gmail.com>
2024-07-03 10:36:29 +0530

    upload image functionality (fix small bug)

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -41,7 +41,7 @@ export default function Profile() {
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
       <form className='flex flex-col gap-4'>
         <input type="file" ref={fileRef} hidden accept='image/*' onChange={(e) => setImage(e.target.files[0])}/>
-        <img src={currentUser.profilePicture} alt="profile" className='h-24 w-24 self-center cursor-pointer rounded-full object-cover mt-2' onClick={() => fileRef.current.click()}/>
+        <img src={formData.profilePicture || currentUser.profilePicture} alt="profile" className='h-24 w-24 self-center cursor-pointer rounded-full object-cover mt-2' onClick={() => fileRef.current.click()}/>
         <p className='text-sm self-center'>{imageError?(<span className='text-red-700'>Error uploading image (file size must be less than 2MB)</span>) : imagePercent > 0 && imagePercent < 100 ? (<span className='text-slate-700'>{`Uploading: ${imagePercent} %`}</span>) : imagePercent === 100 ? (<span className='text-green-700'>Image uploaded successfully</span>) : ('')}</p>
         <input defaultValue={currentUser.username} type="text" id='username' placeholder='Username' className='bg-slate-100 rounded-lg p-3' />
         <input defaultValue={currentUser.email} type="email" id='email' placeholder='Email' className='bg-slate-100 rounded-lg p-3' />
